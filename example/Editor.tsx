e10eee59e2a2173fac8d09945fc423e08d266a38
Alexi Chepura <a.chepura@gmail.com>
2018-11-26 14:52:00 +0300

    first working Strong

--- example/Editor.tsx
+++ example/Editor.tsx
@@ -4,12 +4,14 @@ import { EditorView } from "prosemirror-view"
 import { Schema, DOMParser } from "prosemirror-model"
 import { schema } from "prosemirror-schema-basic"
 import { addListNodes } from "prosemirror-schema-list"
-// import { exampleSetup } from "prosemirror-example-setup"
+import { exampleSetup } from "prosemirror-example-setup"
 
 // import { MenuBar } from "../src/MenuBar"
+import { Strong } from "../src/Strong"
 
 export class Editor extends React.Component<{}> {
   editorNode: HTMLDivElement | null = null
+  view: any
   componentDidMount() {
     // Mix the nodes from prosemirror-schema-list into the basic schema to
     // create a schema with list support.
@@ -22,14 +24,22 @@ export class Editor extends React.Component<{}> {
     domNode.innerHTML = ""
 
     const editorNode = this.editorNode!
-    new EditorView(editorNode, {
+    this.view = new EditorView(editorNode, {
       state: EditorState.create({
-        doc: DOMParser.fromSchema(mySchema).parse(domNode)
-        // plugins: exampleSetup({ schema: mySchema })
+        doc: DOMParser.fromSchema(mySchema).parse(domNode),
+        plugins: exampleSetup({ schema: mySchema, menuBar: false, floatingMenu: false })
       })
     })
   }
+  getView = () => {
+    return this.view
+  }
   render() {
-    return <div ref={r => (this.editorNode = r)} style={{ border: "1px solid gray", minHeight: "200px" }} />
+    return (
+      <div>
+        <Strong mark={schema.marks.strong} getView={this.getView} />
+        <div ref={r => (this.editorNode = r)} style={{ border: "1px solid gray", minHeight: "200px" }} />
+      </div>
+    )
   }
 }
